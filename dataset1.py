5bd72a3178e89636ce1ac1b334378dfdf7addd7c
e195705 <e195705@ie.u-ryukyu.ac.jp>
2021-10-12 16:14:55 +0900

    ex1.3

--- dataset1.py
+++ dataset1.py
@@ -15,7 +15,8 @@ def true_function(x):
     y = np.sin(np.pi * x * 0.8) * 10
     return y
 
-""" fig, ax = plt.subplots()
+""" #描画
+fig, ax = plt.subplots()
 x = np.linspace(-1, 1, 100)
 y = true_function(x)
 
@@ -42,10 +43,44 @@ y = [true_function(i) for i in x]
 df = pd.DataFrame({"観測点": x, "真値": y})
 #print(df)
 
-ax.set_title("ex1.2")
+""" ax.set_title("ex1.2")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 plt.legend("y = sin(π * x * 0.8) * 10")
 plt.scatter(x,y)
 plt.show()
-plt.savefig("ex1.2.png")
+plt.savefig("ex1.2.png") """
+
+"""
+演習1.3
+"""
+noizes = [np.random.normal(0, 2) for _ in range(20)] #平均0, 標準偏差2のノイズを生成
+#print(noizes)
+noizes = [i/2 for i in noizes]
+#print(noizes)
+y = [i+noizes[num] for num,i in enumerate(y)]
+noizes_df = pd.DataFrame({"観測値": y})
+df = pd.concat([df, noizes_df], axis=1)
+#print(df)
+
+#描画
+fig, ax = plt.subplots()
+x = np.linspace(-1, 1, 100)
+y = true_function(x)
+ax.plot(x,y, label="y = sin(π * x * 0.8) * 10")
+
+random.seed(20)
+x = [random.uniform(-1,1) for _ in range(20)] #-1<=x<=1の範囲の乱数を20個生成
+y = [true_function(i) for i in x]
+df = pd.DataFrame({"観測点": x, "真値": y})
+#print(df)
+
+ax.set_title("ex1.3")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+#plt.legend("y = sin(π * x * 0.8) * 10")
+plt.scatter(x,y)
+plt.scatter(x, noizes, label="noizes")
+plt.legend()
+plt.show()
+plt.savefig("ex1.3.png")
